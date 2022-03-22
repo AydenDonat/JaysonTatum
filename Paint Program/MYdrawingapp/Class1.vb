51cce48a4277667d1763058089d07bc355f64550
Ayden Donat <acdon785@learner.dvusd.org>
2022-03-22 12:37:14 -0700

    updated

---
+++ Paint Program/MYdrawingapp/Class1.vb
@@ -0,0 +1,18 @@
+﻿Public Class MyRect
+    Public Property pen As Pen
+    Dim m_image As Image
+    Dim m_a As Point
+    Dim m_b As Point
+
+    Public Sub New(i As Image, a As Point, b As Point)
+        pen = Pens.Red
+        m_image = i
+        m_a = a
+        m_b = b
+    End Sub
+    Public Sub draw()
+        Using g As Graphics = Graphics.FromImage(m_image)
+            g.DrawRectangle(pen, m_a.X, m_a.Y, 500, 100)
+        End Using
+    End Sub
+End Class
